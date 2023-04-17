ca86d337236a23643d5726d3ee6093ae198acf3f
Eliana Carrizo <elikarry22c@gmail.com>
2023-04-16 22:43:53 -0300

    change the icons of the array

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
 const shoppingcar= []
 
 const trips = [{ image: '🌄', code:1, type: 'Mocona Falls', price: 100000},
-               { image: '⛺', code:2, type: 'Jaaukanigas', price: 55000},
+               { image: '🦅', code:2, type: 'Jaaukanigas', price: 55000},
                { image: '⛺', code:3, type: 'El soberbio-Misiones', price: 95000},
-               { image: '⛺', code:4, type: 'The 7 Lakes-Bariloche', price: 355000},
-               { image: '⛺', code:5, type: 'Esteros del Iberá', price: 90000},
-               { image: '⛺', code:6, type: 'Mar del plata', price: 85000},
-               { image: '⛺', code:7, type: 'Nazaret-Portugal', price: 1250000},
-               { image: '⛺', code:8, type: 'Mavericks-California', price: 2118000},
-               { image: '⛺', code:9, type: 'Waimea-Hawaii', price: 2500000},
-               { image: '⛺', code:10, type: 'Gold coast-Australia', price: 2328000}]
+               { image: '🌅', code:4, type: 'The 7 Lakes-Bariloche', price: 355000},
+               { image: '🐊', code:5, type: 'Esteros del Iberá', price: 90000},
+               { image: '🏄🏼‍♀️🌊', code:6, type: 'Mar del plata', price: 85000},
+               { image: '🌊🏄🏼‍♀️', code:7, type: 'Nazaret-Portugal', price: 1250000},
+               { image: '🛹', code:8, type: 'Mavericks-California', price: 2118000},
+               { image: '🌴', code:9, type: 'Waimea-Hawaii', price: 2500000},
+               { image: '🦘', code:10, type: 'Gold coast-Australia', price: 2328000}]
 
 
 const messageInitial = "Select ur trip:"
